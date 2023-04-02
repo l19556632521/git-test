999230fcbef555384c12b5119a07bca91c96ca88
l19556632521 <124578557+l19556632521@users.noreply.github.com>
2023-04-02 15:18:19 +0800

    Update GitTest.java

--- src/main/java/com/itguigu/GitTest.java
+++ src/main/java/com/itguigu/GitTest.java
@@ -13,6 +13,7 @@ public class GitTest {
         System.out.println("hello,git4 master");
         System.out.println("hello,git4 hot-fix");
         System.out.println("hello,git4 push hot-fix");
+        System.out.println("push test");
 
     }
 }
