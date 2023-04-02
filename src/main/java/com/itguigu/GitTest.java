2a9a12013f28bc70319da40c815bf6fad652becd
r2coding <r2coding@qq.com>
2023-04-02 10:37:25 +0800

    second commit git-test

--- src/main/java/com/itguigu/GitTest.java
+++ src/main/java/com/itguigu/GitTest.java
@@ -7,6 +7,7 @@ package com.itguigu;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,git");
+        System.out.println("hello,git1");
 
     }
 }
