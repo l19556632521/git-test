40db450b47796cf54204702db30ec98769f2da03
r2coding <r2coding@qq.com>
2023-04-02 10:34:24 +0800

    first commit git-test

---
+++ src/main/java/com/itguigu/GitTest.java
@@ -0,0 +1,12 @@
+package com.itguigu;
+
+/**
+ * @version 1.0
+ * @learner 李不言
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+
+    }
+}
