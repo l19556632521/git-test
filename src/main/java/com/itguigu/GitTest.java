90df8effaa6c0cb4eafa9e71daca74d5bd36e2bf
r2coding <r2coding@qq.com>
2023-04-02 11:14:53 +0800

    master second commit push git-test

--- src/main/java/com/itguigu/GitTest.java
+++ src/main/java/com/itguigu/GitTest.java
@@ -12,6 +12,7 @@ public class GitTest {
         System.out.println("hello,git3 hot-fix");
         System.out.println("hello,git4 master");
         System.out.println("hello,git4 hot-fix");
+        System.out.println("hello,git4 push hot-fix");
 
     }
 }
